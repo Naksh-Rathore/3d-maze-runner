6e50074f6b67d09c1ee1022e08521d79a588ac9e
Naksh Rathore <naksh.rathore786@gmail.com>
2026-01-11 15:03:44 -0600

    Move shader, shader program, and texture class to material.h

--- include/maze/rendering/material/material.h
+++ include/maze/rendering/material/material.h
@@ -1,16 +1,69 @@
 #pragma once
 
-#include "rendering/material/shader.h"
-#include "rendering/material/shader_program.h"
-#include "rendering/material/texture.h"
+#include <glad/glad.h>
+#include <string>
 
-struct Material {
-    Rendering::Shader m_vert;
-    Rendering::Shader m_frag;
+namespace Rendering {
+    class Shader {
+        private:
+            std::string m_shaderSourceContent{};
+            GLenum m_shaderType{};
+            GLuint m_shader{};
 
-    Rendering::ShaderProgram m_shader;
+            bool m_hasCompiled = false;
 
-    Rendering::Texture2D m_texture;
+            std::string readShader(const std::string& shaderSourceFile);
 
-    Material(const std::string& vertShaderPath, const std::string& fragShaderPath, const std::string& texturePath);
-};
+        public:
+            Shader(const std::string& shaderSourceFile, GLenum shaderType);
+
+            void compileShader();
+
+            GLenum shaderType() const { return m_shaderType; }
+            GLuint shader() const { return m_shader; }
+            std::string shaderSource() const { return m_shaderSourceContent; }
+
+    };
+
+    class ShaderProgram {
+    private:
+        GLuint m_shaderProgram{};
+
+        bool m_hasLinked = false;
+
+    public:
+        ShaderProgram();
+        ~ShaderProgram();
+
+        void link(Shader& vertexShader, Shader& fragmentShader);
+
+        GLuint shaderProgram() const { return m_shaderProgram; }
+    };
+
+
+    class Texture2D {
+        private:
+            GLuint m_texture{};
+        
+            bool m_hasLinked = false;
+
+        public:
+            void link(const std::string& fileName, GLenum wrapOption, GLenum mipMapMinOption, GLenum mipMapMagOption);
+
+            ~Texture2D();
+
+            GLuint texture() { return m_texture; }
+    };
+
+    struct Material {
+        Rendering::Shader m_vert;
+        Rendering::Shader m_frag;
+
+        Rendering::ShaderProgram m_shader;
+
+        Rendering::Texture2D m_texture;
+
+        Material(const std::string& vertShaderPath, const std::string& fragShaderPath, const std::string& texturePath);
+    };
+
+}
--- src/rendering/material/material.cpp
+++ src/rendering/material/material.cpp
@@ -1,9 +1,126 @@
 #include "rendering/material/material.h"
 
-Material::Material(const std::string& vertShaderPath, const std::string& fragShaderPath, const std::string& texturePath) 
+#include <glad/glad.h>
+#include <stb_image.h>
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace Rendering {
+    Shader::Shader(const std::string& shaderSourceFile, GLenum shaderType)
+        : m_shaderSourceContent { readShader(shaderSourceFile) }
+        , m_shaderType { shaderType }
+        , m_shader { glCreateShader(m_shaderType) }
+    {
+    }
+
+    std::string Shader::readShader(const std::string& shaderSourceFile) {
+        std::string shaderContents{};
+        std::string shaderContentsBuffer{};
+    
+        std::ifstream shaderFileStream(shaderSourceFile);
+
+        if (!shaderFileStream.is_open()) {
+            std::cerr << "Failed to open shader file: " << shaderSourceFile << "\n";
+            return "";
+        }
+
+        while (std::getline(shaderFileStream, shaderContentsBuffer))
+            shaderContents += shaderContentsBuffer + "\n";
+
+        shaderFileStream.close();
+        return shaderContents;
+    }
+
+    void Shader::compileShader() {
+        assert(!m_hasCompiled && "ERROR: Shader cannot be compiled twice!");
+
+        m_hasCompiled = true;
+
+        const char *shaderContent { m_shaderSourceContent.c_str() };
+
+        glShaderSource(m_shader, 1, &shaderContent, nullptr);
+        glCompileShader(m_shader);
+
+        int success{};
+        char infoLog[1024]{};
+
+        glGetShaderiv(m_shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(m_shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type " << m_shaderType 
+                    << "\n" << infoLog 
+                    << "\n-- --------------------------------------------------- -- " << std::endl;    
+        }
+    }
+    
+    ShaderProgram::ShaderProgram()
+        : m_shaderProgram { glCreateProgram() }
+    {
+    }
+
+    ShaderProgram::~ShaderProgram() {
+        glDeleteProgram(m_shaderProgram);
+    }
+
+    void ShaderProgram::link(Shader& vertexShader, Shader& fragmentShader) {
+        assert(!m_hasLinked && "ERROR: Shader program cannot be linked twice!");
+
+        m_hasLinked = true;
+
+        glAttachShader(m_shaderProgram, vertexShader.shader());
+        glAttachShader(m_shaderProgram, fragmentShader.shader());
+        glLinkProgram(m_shaderProgram);
+
+        glDetachShader(m_shaderProgram, vertexShader.shader());
+        glDetachShader(m_shaderProgram, fragmentShader.shader());
+    }
+
+    void Texture2D::link(const std::string& fileName, GLenum wrapOption, GLenum mipMapMinOption, GLenum mipMapMagOption) {
+        assert(!m_hasLinked && "ERROR: Texture cannot be linked twice");
+
+        m_hasLinked = true;
+
+        int width, height, nrChannels;
+        stbi_set_flip_vertically_on_load(true);
+    
+        unsigned char *data { stbi_load(fileName.c_str(), &width, &height, &nrChannels, 0) };
+
+        if (data == nullptr) {
+            std::cerr << "Failed to open texture file: " << fileName << "\n";
+            return;
+        }
+
+        glGenTextures(1, &m_texture);
+        glBindTexture(GL_TEXTURE_2D, m_texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapOption);	
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapOption);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, mipMapMinOption);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mipMapMagOption);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
+    }
+
+    Texture2D::~Texture2D() {
+        glDeleteTextures(1, &m_texture);
+    }    
+    
+    Material::Material(const std::string& vertShaderPath, const std::string& fragShaderPath, const std::string& texturePath) 
     : m_vert(vertShaderPath, GL_VERTEX_SHADER)
     , m_frag(fragShaderPath, GL_FRAGMENT_SHADER)
-{
-    m_shader.link(m_vert, m_frag);
-    m_texture.link(texturePath, GL_REPEAT, GL_LINEAR, GL_LINEAR_MIPMAP_LINEAR);
+    {
+        m_vert.compileShader();
+        m_frag.compileShader();
+
+        m_shader.link(m_vert, m_frag);
+        m_texture.link(texturePath, GL_REPEAT, GL_LINEAR, GL_LINEAR_MIPMAP_LINEAR);
+    }
 }
