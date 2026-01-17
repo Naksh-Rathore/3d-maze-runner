e38d816052c4d5b98310662683197d111119fe59
Naksh Rathore <naksh.rathore786@gmail.com>
2026-01-16 18:34:43 -0600

    Change fly camera settings to be faster and less sensitive (personal preference-wise)

--- src/rendering/core/world.cpp
+++ src/rendering/core/world.cpp
@@ -14,7 +14,7 @@
 
 namespace Rendering {
     World::World()
-        : m_camera(glm::vec3(0.0f, 80.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 5.0f, 0.25f)
+        : m_camera(glm::vec3(0.0f, 80.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 15.0f, 0.125f)
 
         , m_planeMesh(CommonVertices::SquareVertices,
                       CommonVertices::SquareIndices,
