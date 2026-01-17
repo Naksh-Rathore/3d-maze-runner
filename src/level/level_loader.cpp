81e94271b1888b02705f513a65794c5ad7ad95ed
Naksh Rathore <naksh.rathore786@gmail.com>
2026-01-16 18:31:11 -0600

    Refactor level loader to have no z-fighting

--- src/level/level_loader.cpp
+++ src/level/level_loader.cpp
@@ -18,26 +18,43 @@ namespace Level {
 
         while (std::getline(levelFile, levelFileContentBuffer))
             levelFileContent += levelFileContentBuffer + "\n";
-
-        glm::vec3 currentPos(-20.0f, 5.0f, -20.0f);
-        glm::vec3 scale(glm::vec3(5.0f, 10.0f, 10.0f));
     
+        const float cell_x = 10.0f;
+        const float cell_z = 10.0f;
+
+        // Wall
+        const float wall_height = 10.0f;
+
+        const float eps = 0.02f;
+
+        const float wall_scale_x = cell_x - eps;
+        const float wall_scale_y = wall_height;
+        const float wall_scale_z = cell_z - eps;
+
+        glm::vec3 currentPos(-20.0f, wall_height * 0.5f, -20.0f);
+
+        glm::vec3 scale(
+            wall_scale_x,
+            wall_scale_y,
+            wall_scale_z
+        );
+
+        const float startX = currentPos.x;
+
         for (char symbol : levelFileContent) {
             if (symbol == '\n') {
-                currentPos.x = -20.0f;
-                currentPos.z += 7.5f;
+                currentPos.x = startX;
+                currentPos.z += cell_z;
                 continue;
             }
 
             if (symbol == '\r')
                 continue;
 
-            std::optional<GameObject::Wall> wall = getWallFromSymbol(symbol, currentPos, scale);
-
-            if (wall.has_value())
+            if (auto wall = getWallFromSymbol(symbol, currentPos, scale))
                 walls.push_back(wall.value());
 
-            currentPos.x += 10.0f;
+            currentPos.x += cell_x;
         }
     }
 
